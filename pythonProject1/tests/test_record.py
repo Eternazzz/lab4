8de54c63b149f6252827b38ce3d825b1a5d22423
eternazzz <alexkonder@mail.ru>
2022-11-11 15:05:46 +0300

    2nd commit

--- pythonProject1/tests/test_record.py
+++ pythonProject1/tests/test_record.py
@@ -10,17 +10,28 @@ def test_record_good():
 
 
 @pytest.fixture()
-def t_calories_calc():
+def t_calories_calc1():
     calories_calc = CaloriesCalculator(1900)
     calories_calc.add_record(Record(amount=400, comment='Завтрак.'))
     calories_calc.add_record(Record(amount=800, comment='Обед.'))
     calories_calc.add_record(Record(amount=600, comment='Ужин.'))
     return calories_calc.get_calories_remained()
 
+@pytest.fixture()
+def t_calories_calc2():
+    calories_calc = CaloriesCalculator(1900)
+    calories_calc.add_record(Record(amount=400, comment='Завтрак.'))
+    calories_calc.add_record(Record(amount=1900, comment='Обед.'))
+    calories_calc.add_record(Record(amount=900, comment='Ужин.'))
+    return calories_calc.get_calories_remained()
 
-def test_calories_calc(t_calories_calc):
-    assert t_calories_calc == 'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более 100 кКал'
 
+def test_calories_calc1(t_calories_calc1):
+    assert t_calories_calc1 == 'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более 100 кКал'
+
+
+def test_calories_calc2(t_calories_calc2):
+    assert t_calories_calc2 == 'Хватит есть!'
 
 @pytest.mark.parametrize("a, result", [(1000, 'Хватит есть!'),
                         (1500, 'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более 300 кКал'),
